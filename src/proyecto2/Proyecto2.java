032cf91fd65b4edeac829289cba87521a2d41d19
Angel <Angel@Angel>
2017-11-01 09:45:25 -0600

    ,k

--- src/proyecto2/Proyecto2.java
+++ src/proyecto2/Proyecto2.java
@@ -15,7 +15,8 @@ public class Proyecto2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application jbljhbjhb,j
+        // TODO code application jbljhbjhb,jjkb,khb
+        //k,bkbh
     }
     
 }
